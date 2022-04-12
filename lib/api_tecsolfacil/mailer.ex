4520d5fd6f99c0c7847a2737982a4863329bb5a3
oteixeiras <fernandos.teixeira@outlook.com>
2022-04-11 21:27:29 -0300

    first commit

---
+++ lib/api_tecsolfacil/mailer.ex
@@ -0,0 +1,3 @@
+defmodule ApiTecsolfacil.Mailer do
+  use Swoosh.Mailer, otp_app: :api_tecsolfacil
+end
---
+++ lib/api_tecsolfacil/repo.ex
@@ -0,0 +1,5 @@
+defmodule ApiTecsolfacil.Repo do
+  use Ecto.Repo,
+    otp_app: :api_tecsolfacil,
+    adapter: Ecto.Adapters.Postgres
+end
